b8a2083441a21a094c5d3a10b5756ba255281401
Tyler Hancock <darklime@live.ca>
2020-07-01 18:43:38 -0600

    Add forgemod.

---
+++ src/main/java/net/darkhax/runelic/Runelic.java
@@ -0,0 +1,13 @@
+package net.darkhax.runelic;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("runelic")
+public class Runelic {
+    
+	/**
+	 * Font ID for the standard Runelic font. Currently supports standard alphanumeric characters.
+	 */
+	public static final ResourceLocation FONT_RUNELIC = new ResourceLocation("runelic", "runelic");
+}
