c38db6d41f66ee8b76d9bf171a2194160f0f356c
Tyler Hancock <darklime@live.ca>
2022-03-10 04:18:17 -0700

    Port to 1.18.2

--- Common/src/main/java/net/darkhax/runelic/Constants.java
+++ Common/src/main/java/net/darkhax/runelic/Constants.java
@@ -1,14 +1,14 @@
 package net.darkhax.runelic;
 
 import net.minecraft.resources.ResourceLocation;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Constants {
 
     public static final String MOD_ID = "runelic";
     public static final String MOD_NAME = "Runelic";
-    public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
     public static final ResourceLocation FONT_RUNELIC = new ResourceLocation("runelic", "runelic");
     public static final ResourceLocation FONT_DEFAULT = new ResourceLocation("minecraft", "default");
