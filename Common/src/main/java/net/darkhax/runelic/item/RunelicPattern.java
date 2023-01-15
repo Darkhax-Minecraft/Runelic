f03e38b4130a6cd18ee95d3357e8b66b794f02bb
Tyler Hancock <darklime@live.ca>
2023-01-15 16:51:34 -0700

    Port to 1.19.3

--- Common/src/main/java/net/darkhax/runelic/item/RunelicPattern.java
+++ Common/src/main/java/net/darkhax/runelic/item/RunelicPattern.java
@@ -4,6 +4,7 @@ import net.darkhax.bookshelf.api.Services;
 import net.darkhax.runelic.Constants;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.BannerPatternItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -13,7 +14,7 @@ import net.minecraft.world.level.block.entity.BannerPattern;
 public class RunelicPattern extends BannerPatternItem {
 
     private static final TagKey<BannerPattern> BANNER_TAG = Services.TAGS.bannerPatternTag(new ResourceLocation(Constants.MOD_ID, "pattern_item/runelic"));
-    private static final Properties PROPERTIES = new Item.Properties().stacksTo(1).rarity(Rarity.EPIC).tab(CreativeModeTab.TAB_DECORATIONS);
+    private static final Properties PROPERTIES = new Item.Properties().stacksTo(1).rarity(Rarity.EPIC);
 
     public RunelicPattern() {
 
--- Forge/src/main/java/net/darkhax/runelic/RunelicForge.java
+++ Forge/src/main/java/net/darkhax/runelic/RunelicForge.java
@@ -1,6 +1,15 @@
 package net.darkhax.runelic;
 
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
 public class RunelicForge {
@@ -8,5 +17,15 @@ public class RunelicForge {
     public RunelicForge() {
 
         new RunelicCommon();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::addItem);
+    }
+
+    private void addItem(CreativeModeTabEvent.BuildContents event) {
+
+        if (event.getTab() == CreativeModeTabs.INGREDIENTS) {
+
+            final Item item = BuiltInRegistries.ITEM.get(new ResourceLocation(Constants.MOD_ID, "runelic_pattern"));
+            event.getEntries().putAfter(new ItemStack(Items.PIGLIN_BANNER_PATTERN), new ItemStack(item), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        }
     }
 }
