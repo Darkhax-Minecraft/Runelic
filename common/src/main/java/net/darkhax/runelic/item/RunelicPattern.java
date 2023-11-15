dc05e96a576a184ada6108663b191f073b0dcc85
Tyler Hancock <darklime@live.ca>
2023-11-14 18:16:56 -0700

    Port to Minecraft 1.20.2 and add support for NeoForge.

--- common/src/main/java/net/darkhax/runelic/item/RunelicPattern.java
+++ common/src/main/java/net/darkhax/runelic/item/RunelicPattern.java
@@ -1,7 +1,7 @@
 package net.darkhax.runelic.item;
 
-import net.darkhax.bookshelf.api.Services;
 import net.darkhax.runelic.Constants;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.BannerPatternItem;
@@ -11,7 +11,7 @@ import net.minecraft.world.level.block.entity.BannerPattern;
 
 public class RunelicPattern extends BannerPatternItem {
 
-    private static final TagKey<BannerPattern> BANNER_TAG = Services.TAGS.bannerPatternTag(new ResourceLocation(Constants.MOD_ID, "pattern_item/runelic"));
+    private static final TagKey<BannerPattern> BANNER_TAG = TagKey.create(Registries.BANNER_PATTERN, new ResourceLocation(Constants.MOD_ID, "pattern_item/runelic"));
     private static final Properties PROPERTIES = new Item.Properties().stacksTo(1).rarity(Rarity.EPIC);
 
     public RunelicPattern() {
---
+++ neoforge/src/main/java/net/darkhax/runelic/RunelicNeoForge.java
@@ -0,0 +1,12 @@
+package net.darkhax.runelic;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class RunelicNeoForge {
+
+    public RunelicNeoForge() {
+
+        new RunelicCommon();
+    }
+}
